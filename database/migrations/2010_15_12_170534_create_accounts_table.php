2e26cb52833a80fd864a371865f079c26beaaf2a
LuizEduardo019 <luizeduardodeoliveira166@gmail.com>
2022-06-10 15:18:26 -0300

    adicionando arquivos

---
+++ database/migrations/2010_15_12_170534_create_accounts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreignId('agency_id')->references('id')->on('agencies');
+            $table->string('account_number', 5);
+            $table->decimal('balance', 8,2);
+            $table->string('password');
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+}
