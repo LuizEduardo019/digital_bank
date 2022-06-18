4fad3176a088405029f280624f04faa8695eec4f
LuizEduardo019 <104370837+LuizEduardo019@users.noreply.github.com>
2022-06-18 19:24:10 -0300

    Continuação do projeto banco

--- app/Http/Controllers/Api/AccountApiController.php
+++ app/Http/Controllers/Api/AccountApiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Account;
+use App\Models\User;
 
 class AccountApiController extends Controller
 {
--- app/Http/Controllers/Api/UserApiController.php
+++ app/Http/Controllers/Api/UserApiController.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use http\Env\Response;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
 use Carbon\Carbon;
 use App\Models\User;
+use App\Models\Address;
+use App\Models\Account;
 
 
 class UserApiController extends Controller
@@ -28,18 +31,37 @@ class UserApiController extends Controller
     }
 
     public function store(Request $request)
-    {
-        $user = new User;
-
+    {        
+        //user
+        $user = new User();
+    
         $user->fill($request->all());  
         $birth_date = $request['birth_date'];
 
         $user->birth_date = Carbon::createFromFormat('d/m/Y', $birth_date)->format('Y/m/d');
-        $user->password = $this->hash::make($user->password);
-    
+        $user->password = md5($user->password);
         $user->save();
+        
+        //account
+        $account = new Account();
 
-        return response()->json(['msg' => 'criado com sucesso!']);
+        $account->user_id = $user->id;
+        $account->fill($request->all());
+        $account->password = md5($user->password);
+        $account->save();
+
+        //address
+        $address = new Address();      
+
+        $address->user_id = $user->id;
+        $address->fill($request->all());
+        $address->save();
+
+        return response()->json(['msg' => 'Conta criada com sucesso', 
+        'data' => ['users' => $user, 'address' => $address, 'account' => $account]]);
+
+        
+        
     }
 
 }
